e97659c350e8fa4baee905a557a86541d299f84f
Alex Bejenaru <alex.d.bejenaru@gmail.com>
2021-10-26 11:05:36 +0300

    added footer social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,11 +13,11 @@ const Footer = () => {
                         </div>
 
                         <div className="social-links">
-                            <FontAwesomeIcon className="social-links_badge" icon={ faFacebook } />
-                            <FontAwesomeIcon className="social-links_badge" icon={ faYoutube } />
-                            <FontAwesomeIcon className="social-links_badge" icon={ faInstagram } />
-                            <FontAwesomeIcon className="social-links_badge" icon={ faPinterest } />
-                            <FontAwesomeIcon className="social-links_badge" icon={ faTwitter } />
+                            <a href="https://www.facebook.com"><FontAwesomeIcon className="social-links_badge" icon={ faFacebook } /></a>
+                            <a href="https://www.youtube.com"><FontAwesomeIcon className="social-links_badge" icon={ faYoutube } /></a>
+                            <a href="https://www.instagram.com"><FontAwesomeIcon className="social-links_badge" icon={ faInstagram } /></a>
+                            <a href="https://www.pinterest.com"><FontAwesomeIcon className="social-links_badge" icon={ faPinterest } /></a>
+                            <a href="https://www.twitter.com"><FontAwesomeIcon className="social-links_badge" icon={ faTwitter } /></a>
                         </div>
                     </div>
 
